0dc897499775059aaeebe267c7996751ca89cefe
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-01-28 15:53:20 +0100

    Detecting button clicks

--- JavaScript programs/home.js
+++ JavaScript programs/home.js
@@ -1,11 +1,24 @@
-let mySymbol =Symbol();
+/*let mySymbol =Symbol();
 let person={
     name:'',
     age: 33,
     [mySymbol]:'secretInformation',
-    showInfo: function()
+    showInfo: function(realAge)
     {
-        showMessage(this.name + '' +realAge)
+        showMessage(this.name + 'is' +realAge)
     }
 
 };
+person.showInfo(34);
+
+
+let today= new Date();
+showMessage(today.toDate());
+*/
+
+/*let a ="Hello";
+showMessage(a.charA(0));
+
+a.style.fontWeight='700'; */
+
+let a =documnt,getElementById
