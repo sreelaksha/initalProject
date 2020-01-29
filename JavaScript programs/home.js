857a3dd6077cfed748a5595c7de6e23e3919e3ee
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-01-29 16:19:04 +0100

    array concept

--- JavaScript programs/home.js
+++ JavaScript programs/home.js
@@ -15,10 +15,33 @@ person.showInfo(34);
 let today= new Date();
 showMessage(today.toDate());
 */
-
+//
 /*let a ="Hello";
 showMessage(a.charA(0));
 
 a.style.fontWeight='700'; */
 
-let a =documnt,getElementById
+ //
+
+/* var button = document.getElementById('click-me');
+button.addEventListener('click',function()
+{
+        const review = document.getElementById('review');
+
+        if(review.classList.contains('d-none'))
+        {
+            review.classList.remove('d-none');
+            button.textContent = 'CLOSE Review';
+        }
+        else
+        {
+            review.classList.add('d-none');
+            button.textContent = 'SEE Review';
+        }
+
+
+}); */
+
+ const values=['a','b','c'];
+ values.push('d');
+ console.log(values);
