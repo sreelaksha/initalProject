99f0d7cb5cae758221a005e7a1bddff2435b9675
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-02-06 11:17:38 +0100

    Selecting and Modifying Dom elements

--- webpack/src/scripts/index.js
+++ webpack/src/scripts/index.js
@@ -21,3 +21,37 @@ let intervalId= setInterval (function(){
     console.log('1 Second please');},1000);
 */
 //clearInterval(intervalId);
+
+
+//The location object
+//console.log(document.location.href);
+
+//Selecting DOM elements
+/*
+let element1= document.getElementById('first');
+console.log(element1);
+
+let element2= document.getElementsByClassName('p1');
+console.log(element2[1]); //can give index to search for a position
+
+let element3= document.getElementsByTagName('p');
+console.log(element3);
+*/
+
+//Modifying DOM elements
+/*let element=document.getElementById('first');
+element.textContent = 'This is new content.... ';
+element.setAttribute('foo', 'fooValue');
+console.log(element);
+element.classList.add('p2');
+element.style.color = 'blue';*/
+
+
+
+
+
+
+
+
+
+
