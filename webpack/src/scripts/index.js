e7afcb3d159a314468294582ee381616feb95f4c
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-02-12 10:35:08 +0100

    Using constructor function to create objects

--- webpack/src/scripts/index.js
+++ webpack/src/scripts/index.js
@@ -1,4 +1,38 @@
-'use strict' ;
+//JavaScript Objects and Prototypes
 
-var dog = {name: 'Duggu' , color: 'brown' , age:'8'};
+//Dynamic nature of JavaScript
+//'use strict' ;
+/*
+var dog = {
+    name: 'Duggu' ,
+    color: 'brown' ,
+    age:'8',
+    sister:function(){ console.log('Laksha');}
+};
+dog.breed= 'Boxer';
 console.log(dog.name);
+dog.speak = function()
+{
+   console.log('boww boww');
+};
+console.log(dog.breed);
+dog.speak();
+dog.sister();
+*/
+
+
+//Using Constructor Functions to create JavaScript Objects
+/*
+function Cat(name,color)
+{
+    this.name= name;
+    this.color= color;
+}
+
+//var cat= Cat(); //return the value of Cat() function
+//console.log(window.name);
+var cat= new Cat('Max','Brown');
+console.log(cat);*/
+
+
+//Using Objects.create() to create JavaScript Objects
